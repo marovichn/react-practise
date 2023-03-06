5def1a9f8331aefa1778eccc222a04faa8734d82
marovichn <nikolamarovic.11@gmail.com>
2023-03-06 01:14:18 +0100

    improvement

--- src/components/Users/AddedUserList.js
+++ src/components/Users/AddedUserList.js
@@ -3,15 +3,18 @@ import Card from "../UI/Card";
 import style from "./AddedUserList.module.css";
 
 const AddedUserList = (props) => {
-  return props.users.map((user) => {
-    return (
-      <Card key={user.key} className={style.users}>
-        <ul>
-          <li>Name: {user.username}</li>
-          <li>Age: {user.age}</li>
-        </ul>
-      </Card>
-    );
-  });
+  return (
+    <Card className={style.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li key={user.key}>
+              {user.username} ({user.age} years old)
+            </li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
 };
 export default AddedUserList;
